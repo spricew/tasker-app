d3937d9e6f6c2d34dfc5869206bceaba91a5d584
Heyder Medina <medinaheyder@gmail.com>
2026-03-20 00:11:20 -0600

    feat: add PrimaryButton component with styled button

---
+++ app/components/ui/PrimaryButton.tsx
@@ -0,0 +1,5 @@
+export default function PrimaryButton() {
+    return (
+        <button className="bg-primary-container text-white rounded-full px-4 py-2 text-lg font-medium tracking-wider uppercase">Iniciar sesion</button>
+    );
+}
