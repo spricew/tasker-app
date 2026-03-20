6d52bfaf379e2bad7ac9cd00e01c025377ed5b2e
Heyder Medina <medinaheyder@gmail.com>
2026-03-20 00:26:49 -0600

    refactor: refactor button className for improved formatting

--- app/components/ui/PrimaryButton.tsx
+++ app/components/ui/PrimaryButton.tsx
@@ -4,10 +4,9 @@ interface PrimaryButtonProps {
 
 export default function PrimaryButton({ text }: PrimaryButtonProps) {
     return (
-        <button className="
+        <button className="inline-flex items-center justify-center
         rounded-full px-4 py-2
-        text-lg font-medium
-        bg-primary-container text-white">
+        text-lg font-medium bg-primary-container text-white">
             {text}
         </button>
     );
