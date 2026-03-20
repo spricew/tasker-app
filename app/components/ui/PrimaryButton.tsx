62c034f45cc44cc1589e2ef11ec2758bfc29b547
Heyder Medina <medinaheyder@gmail.com>
2026-03-20 12:26:45 -0600

    refactor: adjust button colors to follow theme

--- app/components/ui/PrimaryButton.tsx
+++ app/components/ui/PrimaryButton.tsx
@@ -5,8 +5,8 @@ interface PrimaryButtonProps {
 export default function PrimaryButton({ text }: PrimaryButtonProps) {
     return (
         <button className="inline-flex items-center justify-center
-        rounded-full px-4 py-2
-        text-lg font-medium bg-primary-container text-white">
+        rounded-full px-4 py-2 bg-primary-container
+        text-lg font-medium tracking-tight text-on-primary-container">
             {text}
         </button>
     );
