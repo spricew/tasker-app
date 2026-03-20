df9269f90a95521878b19c51d16412116bf72399
Heyder Medina <medinaheyder@gmail.com>
2026-03-20 00:10:29 -0600

    refactor: custom input component renamed

--- app/components/ui/PrimaryInput.tsx
+++ app/components/ui/PrimaryInput.tsx
@@ -6,10 +6,11 @@ interface InputProps {
 export default function PrimaryInput({ label }: InputProps) {
     return (
         <div className="flex flex-col gap-2">
-            {label && (<label className="text-base font-medium text-neutral-500">{label}</label>)}
+            {label && (<label className="text-base font-medium uppercase text-neutral-500">{label}</label>)}
             <input
                 type="text"
                 placeholder="text"
+                className="py-2 px-6 rounded-full ring ring-outline-variant bg-surface-container"
             />
         </div>
     );
