6fbf9fc9134d3a92648dea4ec930e05f4690bfdb
Heyder Medina <medinaheyder@gmail.com>
2026-03-20 12:27:31 -0600

    refactor: sign up link fixed and link styles updated

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import PrimaryButton from "@/app/components/ui/PrimaryButton";
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center">
-      <main className="flex flex-col gap-2">
+      <main className="flex flex-col gap-2 p-12 rounded-3xl bg-surface-container">
         <div className="flex flex-col">
           <div className="size-20 bg-zinc-900 rounded-full mb-2" />
           <span className="text-3xl font-semibold tracking-tighter">
@@ -16,7 +16,7 @@ export default function Home() {
           <PrimaryInput label="email" />
           <PrimaryInput label="contraseña" />
           <PrimaryButton text="Iniciar sesión" />
-          <span className="text-sm text-zinc-500">¿No tienes una cuenta? <a href="#" className="text-zinc-900">Crear cuenta</a></span>
+          <span className="text-sm text-zinc-500">¿Nuevo en Tasker? <a href="/SignUp" className="text-primary">Crea una cuenta</a></span>
         </form>
       </main>
     </div>
