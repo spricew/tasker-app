f5963988a207382320fbcd2be96c2cc7cb37dfcd
Heyder Medina <medinaheyder@gmail.com>
2026-03-20 12:28:17 -0600

    refactor: sign up card layout updated

--- app/SignUp/page.tsx
+++ app/SignUp/page.tsx
@@ -1,17 +1,25 @@
-import PrimaryButton from "../components/ui/PrimaryButton";
-import PrimaryInput from "../components/ui/PrimaryInput";
+import PrimaryButton from "@/app/components/ui/PrimaryButton";
+import PrimaryInput from "@/app/components/ui/PrimaryInput";
 
 export default function SignUp() {
     return (
-        <div className="flex justify-center items-center">
-            <main className="flex flex-col w-120 h-140 p-12 rounded-3xl bg-surface-container">
-                <h1 className="text-3xl font-semibold tracking-tighter">Crear una cuenta</h1>
-                <form>
+        <div className="flex justify-center flex-1 items-center">
+            <main className="flex flex-col items-center gap-2 w-120 h-140 p-10 rounded-3xl bg-surface-container-low">
+                <header>
+                    <h1 className="text-3xl text-center font-semibold tracking-tighter">Crear cuenta</h1>
+                    <span className="text-sm text-ref-palette-neutral-50 mr-1">
+                        ¿Ya tienes una cuenta?
+                    </span>
+                    <a href="/" className="text-sm text-primary font-medium tracking-tight">Inicia sesión</a>
+                </header>
+                <form className="flex flex-col gap-2 w-full">
                     <PrimaryInput label="usuario" />
                     <PrimaryInput label="email" />
                     <PrimaryInput label="contraseña" />
-                    <PrimaryButton text="Registrarse" />
+                    <PrimaryInput label="Teléfono" />
+                    <PrimaryButton text="Crear cuenta" />
                 </form>
+
             </main>
         </div>
     );
