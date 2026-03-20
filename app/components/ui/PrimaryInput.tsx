f535d8a285949e303b82cfd4d7a37ae83bfa18ff
Heyder Medina <medinaheyder@gmail.com>
2026-03-20 17:10:47 -0600

    refactor: extend InputProps to include input attributes and adjust input padding

--- app/components/ui/PrimaryInput.tsx
+++ app/components/ui/PrimaryInput.tsx
@@ -1,16 +1,17 @@
 
-interface InputProps {
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
     label?: string;
 }
 
-export default function PrimaryInput({ label }: InputProps) {
+export default function PrimaryInput({ label, ...props }: InputProps) {
     return (
         <div className="flex flex-col gap-1.5">
             {label && (<label className="text-base font-medium capitalize text-neutral-500">{label}</label>)}
             <input
                 type="text"
                 placeholder="text"
-                className="py-2 px-6 rounded-full ring ring-outline-variant bg-surface-container-lowest"
+                className="py-2 px-4 rounded-full ring ring-outline-variant bg-surface-container-lowest"
+                {...props}
             />
         </div>
     );
