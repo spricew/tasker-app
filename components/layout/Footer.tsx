9bd0cff9ea58d5936a74e184f66a500e0db106c1
Heyder Medina <medinaheyder@gmail.com>
2026-03-21 13:38:29 -0600

    feat: footer component created with a variant

---
+++ components/layout/Footer.tsx
@@ -0,0 +1,11 @@
+
+
+export default function Footer() {
+    return (
+        <footer className="flex items-center h-fit w-full p-24 bg-red-700">
+            <div className="flex flex-col gap-y-2">
+                asd
+            </div>
+        </footer>
+    );
+}
---
+++ components/layout/FooterVariant.tsx
@@ -0,0 +1,7 @@
+export default function FooterVariant() {
+    return (
+        <footer className="flex items-center justify-center py-6 text-base bg-surfaceContainer text-onSurface">
+            ©Enterprise 2026. Todos los derechos reservados.
+        </footer>
+    );
+}
