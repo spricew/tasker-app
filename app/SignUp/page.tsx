07227dffac576e904b5f32b58ef6860250dca15a
Heyder Medina <medinaheyder@gmail.com>
2026-03-21 00:03:41 -0600

    refactor: layout updated

--- app/SignUp/page.tsx
+++ app/SignUp/page.tsx
@@ -4,15 +4,15 @@ import PrimaryInput from "@/app/components/ui/PrimaryInput";
 export default function SignUp() {
     return (
         <div className="flex justify-center flex-1 items-center">
-            <main className="flex flex-col items-center gap-2 w-120 h-140 p-10 rounded-3xl bg-surface-container-low">
-                <header>
+            <main className="flex flex-col justify-center items-center gap-2 w-120 h-140 p-10 rounded-3xl bg-surface-container-low">
+                <header className="mb-2">
                     <h1 className="text-3xl text-center font-semibold tracking-tighter">Crear cuenta</h1>
                     <span className="text-sm text-ref-palette-neutral-50 mr-1">
                         ¿Ya tienes una cuenta?
                     </span>
                     <a href="/" className="text-sm text-primary font-medium tracking-tight">Inicia sesión</a>
                 </header>
-                <form className="flex flex-col gap-2 w-full">
+                <form className="flex flex-col gap-3 w-full">
                     <PrimaryInput label="usuario" placeholder="userexample"/>
                     <PrimaryInput label="email" placeholder="email@example.com"/>
                     <PrimaryInput label="contraseña" placeholder="••••••••" type="password"/>
