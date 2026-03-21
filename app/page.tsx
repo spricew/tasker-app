b0626b6cadbc32b37e29b799edca3afd26919f27
Heyder Medina <medinaheyder@gmail.com>
2026-03-21 00:04:41 -0600

    refactor: card background color adjusted

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import PrimaryButton from "@/app/components/ui/PrimaryButton";
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center">
-      <main className="flex flex-col gap-2 p-12 rounded-3xl bg-surface-container">
+      <main className="flex flex-col gap-2 p-12 rounded-3xl bg-surface-container-low">
         <div className="flex flex-col">
           <div className="size-20 bg-zinc-900 rounded-full mb-2" />
           <span className="text-3xl font-semibold tracking-tighter">
