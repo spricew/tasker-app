eecb0742434a68f9b1a3a86b7ce134d11dc6ecb4
Heyder Medina <medinaheyder@gmail.com>
2026-03-21 00:13:29 -0600

    refactor: page structure updated

--- app/page.tsx
+++ app/page.tsx
@@ -4,20 +4,23 @@ import PrimaryButton from "@/app/components/ui/PrimaryButton";
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center">
-      <main className="flex flex-col gap-2 p-12 rounded-3xl bg-surface-container-low">
-        <div className="flex flex-col">
-          <div className="size-20 bg-zinc-900 rounded-full mb-2" />
-          <span className="text-3xl font-semibold tracking-tighter">
-            Tasklist
-          </span>
-          <span className="text-base font-light tracking-tight">Eleva tu productividad</span>
-        </div>
-        <form className="flex flex-col w-100 gap-2" action="">
+      <main className="flex flex-col gap-4 p-12 rounded-3xl bg-surface-container-low">
+        <header className="flex flex-col">
+          <div className="flex flex-col gap-2">
+            <img src="/logo.png" alt="tasklist logo" className="w-16 object-center" />
+            <span className="text-3xl font-semibold tracking-tighter">
+              Tasker
+            </span>
+          </div>
+
+          <span className="text-base font-light">Eleva tu productividad</span>
+        </header>
+        <form className="flex flex-col w-100 gap-3" action="">
           <PrimaryInput label="email" placeholder="email@example.com" />
           <PrimaryInput label="contraseña" placeholder="••••••••" type="password" />
           <PrimaryButton text="Iniciar sesión" />
-          <span className="text-sm text-zinc-500">¿Nuevo en Tasker? <a href="/SignUp" className="text-primary">Crea una cuenta</a></span>
         </form>
+        <span className="text-sm text-zinc-500">¿Nuevo en Tasker? <a href="/SignUp" className="text-primary">Crea una cuenta</a></span>
       </main>
     </div>
   );
