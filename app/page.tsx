b57d55d328fc443960fa5173523a7d89d5208697
Heyder Medina <medinaheyder@gmail.com>
2026-03-21 12:42:41 -0600

    refactor: login page moved

--- app/page.tsx
+++ app/page.tsx
@@ -4,29 +4,8 @@ import PrimaryButton from "@/app/components/ui/PrimaryButton";
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center">
-      <main className="squircle flex flex-col gap-4 p-12 rounded-3xl bg-surface-container-low">
-        <header className="flex flex-col">
-          <div className="flex flex-col gap-2">
-            <img src="/logo.png" alt="tasklist logo" className="w-16 object-center" />
-            <span className="text-3xl font-semibold tracking-tighter">
-              Tasker
-            </span>
-          </div>
-
-          <span className="text-base font-light">Eleva tu productividad</span>
-        </header>
-        <form className="flex flex-col w-100 gap-3" action="">
-          <PrimaryInput label="email" placeholder="email@example.com" />
-          <PrimaryInput label="contraseña" placeholder="••••••••" type="password" />
-          <PrimaryButton text="Iniciar sesión" />
-        </form>
-        <div>
-          <span className="text-sm text-ref-palette-neutral-50 mr-1">
-            ¿Nuevo en Tasker?
-          </span>
-          <a href="/register" className="text-sm text-primary font-medium tracking-tight">Crea una cuenta</a>
-        </div>
-      </main>
+      
+      <a href="/login" className="text-2xl">ir al login</a>
     </div>
   );
 }
