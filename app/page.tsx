928209bdc07e76380e23eecc8217de8b0ca9b620
Heyder Medina <medinaheyder@gmail.com>
2026-03-21 13:38:43 -0600

    refactor: home cleaned

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,7 @@
-import PrimaryInput from "@/app/components/ui/PrimaryInput";
-import PrimaryButton from "@/app/components/ui/PrimaryButton";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center">
-      
-      <a href="/login" className="text-2xl">ir al login</a>
+    <div className="flex flex-col flex-1">
     </div>
   );
 }
