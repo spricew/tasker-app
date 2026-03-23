a7cb2bd46c1e1fe359505b3a14efd0608aac2307
Heyder Medina <medinaheyder@gmail.com>
2026-03-22 23:31:28 -0600

    feat: student dashboard page created

--- app/(dashboard)/student/page.tsx
+++ app/(dashboard)/student/page.tsx
@@ -1,5 +1,7 @@
-// export default function Student() {
-//     return(
-
-//     );
-// }
+export default function Student() {
+    return (
+        <div className="flex flex-col flex-1">
+            <h1>Student Dashboard</h1>
+        </div>
+    );
+}
