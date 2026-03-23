83918c3b765b43ddc8082d75399cd6d9d5ffd030
Heyder Medina <medinaheyder@gmail.com>
2026-03-23 11:51:20 -0600

    feat: table structure created to get all users in the plataform

--- app/(dashboard)/admin/page.tsx
+++ app/(dashboard)/admin/page.tsx
@@ -2,8 +2,8 @@ import PrimaryButton from "@/components/ui/PrimaryButton";
 
 export default function Adminr() {
     return (
-        <div className="flex flex-col flex-1 px-18 py-12">
-            <div className="flex justify-between">
+        <div className="flex flex-col flex-1 gap-y-8 px-18 py-12">
+            <header className="flex justify-between items-center">
                 <div>
                     <h1 className="text-4xl tracking-tighter font-bold">Admin Dashboard</h1>
                     <p>Officia proident ut dolore magna qui consectetur mollit et id enim fugiat sint nisi labore.</p>
@@ -11,8 +11,50 @@ export default function Adminr() {
 
                 <PrimaryButton
                     text="Crear usuario"
+                    glow
                 />
+            </header>
+
+
+            <div className="squircle rounded-2xl bg-surface-container-lowest shadow-xs">
+                <table className="w-full text-sm text-left rtl:text-right text-body">
+                    <thead className="text-sm border-b border-outline-variant">
+                        <tr>
+                            <th scope="col" className="px-6 py-5 font-medium">
+                                Nombre completo
+                            </th>
+                            <th scope="col" className="px-6 py-5 font-medium">
+                                Email
+                            </th>
+                            <th scope="col" className="px-6 py-5 font-medium">
+                                Rol
+                            </th>
+                            <th scope="col" className="px-6 py-5 font-medium text-center">
+                                Actions
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr className="border-b border-outline-variant nth-last-[1]:border-0">
+                            <th scope="row" className="px-6 py-4 font-medium text-heading whitespace-nowrap">
+                                John Doe
+                            </th>
+                            <td className="px-6 py-4">
+                                john@example.com
+                            </td>
+                            <td className="px-6 py-4">
+                                Admin
+                            </td>
+                            <td className="flex justify-center px-6 py-4 gap-2">
+                                <PrimaryButton text="editar" theme="secondary" />
+                                <PrimaryButton text="eliminar" theme="destructive" />
+                            </td>
+                        </tr>
+                    </tbody>
+                    
+                </table>
             </div>
+
         </div>
     );
 }
