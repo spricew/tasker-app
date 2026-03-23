de9be686f4b93e5bc69b8e66f245cbfc7f64328d
Heyder Medina <medinaheyder@gmail.com>
2026-03-22 23:30:24 -0600

    feat: add navigation links to Home component

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,9 @@
 export default function Home() {
   return (
     <div className="flex flex-col flex-1">
+      <a href="/student" className="text-2xl">Student</a>
+      <a href="/admin" className="text-2xl">Admin</a>
+      <a href="/login" className="text-2xl">Login</a>
     </div>
   );
 }
