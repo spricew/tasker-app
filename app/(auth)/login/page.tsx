096aaecc8296a9c451b85b3682bfca1b4b83a00e
Heyder Medina <medinaheyder@gmail.com>
2026-03-30 17:51:20 -0600

    feat: enhance login functionality with user role redirection and loading state management

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,8 +1,61 @@
-import PrimaryInput from "@/components/ui/PrimaryInput";
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { loginUser } from "@/lib/api/auth";
+import { sileo } from "sileo";
 import PrimaryButton from "@/components/ui/Buttons/PrimaryButton";
-import TertiaryButton from "@/components/ui/Buttons/TertiaryButton";
+import PrimaryInput from "@/components/ui/PrimaryInput";
 
 export default function Login() {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSubmit = async (e: React.SyntheticEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    const formData = new FormData(e.currentTarget);
+    const email = formData.get("email") as string;
+    const password = formData.get("password") as string;
+
+    try {
+      const respuesta = await loginUser({ email, password });
+
+      sileo.success({
+        title: "Sesión iniciada correctamente",
+        duration: 3000,
+        autopilot: {
+          expand: 0,
+          collapse: 2000,
+        },
+      });
+
+      if (respuesta.usuario.rol === 'ADMIN') {
+        router.push("/admin");
+      } else {
+        router.push("/student");
+      }
+
+    } catch (error: any) {
+      sileo.error({
+        title: "Error al iniciar sesión",
+        duration: 4500,
+        autopilot: {
+          expand: 0,
+          collapse: 3500,
+        },
+        description: (
+          <span className="text-white font-medium">
+            {error.message}
+          </span>
+        ),
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <div className="flex flex-col flex-1 items-center justify-center">
       <main className="squircle flex flex-col gap-4 p-12 rounded-3xl bg-surface-container-low">
@@ -16,16 +69,15 @@ export default function Login() {
 
           <span className="text-base font-light">Eleva tu productividad</span>
         </header>
-        <form className="flex flex-col w-100 gap-3">
-          <PrimaryInput label="email" placeholder="email@example.com" />
-          <PrimaryInput label="contraseña" placeholder="••••••••" type="password" />
-          <PrimaryButton text="Iniciar sesión" extraclass="w-full"/>
+        <form className="flex flex-col w-100 gap-3" onSubmit={handleSubmit}>
+          <PrimaryInput name="email" label="Correo Electrónico" placeholder="admin@example.com" />
+          <PrimaryInput name="password" label="Contraseña" type="password" placeholder="••••••••" />
+          <PrimaryButton text={isLoading ? "Cargando..." : "Iniciar sesión"} extraclass="w-full" />
         </form>
         <div>
           <span className="text-sm text-ref-palette-neutral-50">
             ¿Nuevo en Tasker?
           </span>
-          <TertiaryButton text="Crea una cuenta" theme="primary" href="/register" extraclass="text-sm" />
         </div>
       </main>
     </div>
