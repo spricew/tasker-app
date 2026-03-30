8b3cde72924056fa4e77dc17b4973a1ed445cafb
Heyder Medina <medinaheyder@gmail.com>
2026-03-30 16:16:05 -0600

    feat: add dashboard icon to Admin page and update layout for improved header presentation

--- app/(dashboard)/admin/page.tsx
+++ app/(dashboard)/admin/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import UsersTable from "@/components/layout/UsersTable";
 import CreateUserModal from "@/components/layout/CreateUserModal";
 
@@ -5,14 +6,17 @@ export default function Admin() {
     return (
         <div className="flex flex-col flex-1 gap-y-8 px-18 py-12">
             <header className="flex justify-between items-center">
-                <div>
-                    <h1 className="text-4xl tracking-tighter font-semibold">Admin Dashboard</h1>
-                    <p>Gestión completa de estudiantes y administradores del sistema.</p>
+                <div className="flex items-center gap-x-5">
+                    <Image src={"/images/dashboard-icon.png"} alt="" width={65} height={65} className="drop-shadow-xl" />
+                    <div>
+                        <h1 className="text-4xl tracking-tighter font-semibold">Admin Dashboard</h1>
+                        <p>Gestión completa de estudiantes y administradores del sistema.</p>
+                    </div>
                 </div>
-                
+
                 <CreateUserModal />
             </header>
-            
+
             <UsersTable />
         </div>
     );
