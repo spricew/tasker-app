e9c4addec589e781f8adf297fb960afa4cf048d9
Heyder Medina <medinaheyder@gmail.com>
2026-04-02 12:50:52 -0600

    refactor: removed unused line

--- components/ui/TableRow.tsx
+++ components/ui/TableRow.tsx
@@ -15,7 +15,6 @@ export default function TableRow({ id, name, email, role, isCurrentUser }: Table
     return (
         <tr className={`border-b border-outline-variant nth-last-[1]:border-0 ${isCurrentUser ? "bg-ref-palette-primary-90/30" : "bg-surface-container-lowest"}`}>
             <td scope="row" className="px-6 py-4 font-medium whitespace-nowrap capitalize">
-                {/* {name} {isCurrentUser && <span className="text-primary font-semibold ml-1">(Tú)</span>} */}
                 {name} {isCurrentUser && <Pill text="You" />}
             </td>
             <td className="px-6 py-4">
