5fb8932c2bb376c67cedc9bb04251ca02b58671d
Heyder Medina <medinaheyder@gmail.com>
2026-04-07 16:39:04 -0600

    feat: enhance CreateTaskButton component with animated modal for task creation and improved close button functionality

--- components/ui/Buttons/CreateTaskbutton.tsx
+++ components/ui/Buttons/CreateTaskbutton.tsx
@@ -7,7 +7,9 @@ import { createTask } from "@/lib/api/tasks";
 import PrimaryButton from "./PrimaryButton";
 import TertiaryButton from "./TertiaryButton";
 import PrimaryInput from "../PrimaryInput";
-import { Plus } from "lucide-react";
+import { Plus, X } from "lucide-react";
+
+import { motion, AnimatePresence } from 'framer-motion';
 
 export default function CreateTaskButton() {
     const router = useRouter();
@@ -69,33 +71,62 @@ export default function CreateTaskButton() {
                 onClick={() => setShowModal(true)}
             />
 
-            {showModal && (
-                <div className="fixed inset-0 grid place-items-center w-full h-full overflow-y-hidden bg-black/50 z-100">
-                    <div className="squircle flex flex-col gap-4 w-130 max-w-[90vw] p-10 rounded-3xl bg-surface-container-low">
-                        <header className="flex flex-col">
-                            <div className="flex justify-between items-start">
-                                <span className="text-3xl font-semibold tracking-tighter">Agregar tarea</span>
-                                <button onClick={() => setShowModal(false)} className="text-gray-500 hover:text-gray-800">
-                                    ✕
-                                </button>
-                            </div>
+            <AnimatePresence>
+                {showModal && (
 
-                            <span className="text-base font-light">Ingresa el nombre para tu nueva tarea</span>
-                        </header>
+                    <motion.div
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        transition={{ duration: 0.3 }}
+                        exit={{ opacity: 0 }}
+                        className="fixed inset-0 grid place-items-center w-full h-full overflow-y-hidden bg-black/50 z-100"
+                    >
 
-                        <form className="flex flex-col w-full gap-3" onSubmit={handleSubmit}>
-                            <PrimaryInput name="title" label="Nombre de la tarea" placeholder="Nueva tarea" required />
+                        <motion.div
+                            initial={{ opacity: 0, scale: 0 }}
+                            animate={{ opacity: 1, scale: 1 }}
+                            transition={{ type: 'spring', stiffness: 400, damping: 25 }}
 
-                            <PrimaryButton
-                                text={isLoading ? "Guardando..." : "Crear tarea"}
-                                extraclass="w-full"
-                                type="submit"
-                                disabled={isLoading}
-                            />
-                        </form>
-                    </div>
-                </div>
-            )}
+                            exit={{
+                                scale: 0,
+                                opacity: 0,
+                                transition: {
+                                    type: "tween",
+                                    duration: 0.15,
+                                    ease: "easeOut"
+                                }
+                            }}
+
+                            className="squircle flex flex-col gap-4 w-130 max-w-[90vw] p-10 rounded-3xl bg-surface-container-low"
+                        >
+
+                            <header className="flex flex-col">
+                                <div className="flex justify-between items-start">
+                                    <span className="text-3xl font-semibold tracking-tighter">Agregar tarea</span>
+                                    <TertiaryButton
+                                        Icon={<X strokeWidth={2.4} className="size-[1.1em]" />}
+                                        onClick={() => setShowModal(false)}
+                                        extraclass="text-ref-palette-neutral-50"
+                                    />
+                                </div>
+
+                                <span className="text-base font-light">Ingresa el nombre para tu nueva tarea</span>
+                            </header>
+
+                            <form className="flex flex-col w-full gap-3" onSubmit={handleSubmit}>
+                                <PrimaryInput name="title" label="Nombre de la tarea" placeholder="Nueva tarea" required />
+
+                                <PrimaryButton
+                                    text={isLoading ? "Guardando..." : "Crear tarea"}
+                                    extraclass="w-full"
+                                    type="submit"
+                                    disabled={isLoading}
+                                />
+                            </form>
+                        </motion.div>
+                    </motion.div>
+                )}
+            </AnimatePresence>
         </>
     );
 }
