631a4c1f664cd0595f01f020678d48438aedf471
Heyder Medina <medinaheyder@gmail.com>
2026-04-10 16:44:07 -0600

    refactor: replace modal implementation in CreateTaskButton with AnimatedModal for improved code organization and readability

--- components/ui/Buttons/CreateTaskbutton.tsx
+++ components/ui/Buttons/CreateTaskbutton.tsx
@@ -10,6 +10,7 @@ import PrimaryInput from "../PrimaryInput";
 import { Plus, X } from "lucide-react";
 
 import { motion, AnimatePresence } from 'framer-motion';
+import AnimatedModal from "../AnimatedModal";
 
 export default function CreateTaskButton() {
     const router = useRouter();
@@ -71,61 +72,25 @@ export default function CreateTaskButton() {
                 onClick={() => setShowModal(true)}
             />
 
-            <AnimatePresence>
-                {showModal && (
 
-                    <motion.div
-                        initial={{ opacity: 0 }}
-                        animate={{ opacity: 1 }}
-                        transition={{ duration: 0.3 }}
-                        exit={{ opacity: 0 }}
-                        className="fixed inset-0 grid place-items-center w-full h-full overflow-y-hidden bg-black/50 z-100"
-                    >
+            <AnimatedModal
+                title="Añadir nueva tarea"
+                description="Ingresa el nombre de la nueva tarea"
+                onClose={() => setShowModal(false)}
+                isOpen={showModal}
+            >
 
-                        <motion.div
-                            initial={{ opacity: 0, scale: 0 }}
-                            animate={{ opacity: 1, scale: 1 }}
-                            transition={{ type: 'spring', stiffness: 400, damping: 25 }}
-                            exit={{
-                                scale: 0.95,
-                                opacity: 0,
-                                transition: {
-                                    type: "tween",
-                                    duration: 0.1,
-                                    ease: "easeOut"
-                                }
-                            }}
+                <form className="flex flex-col w-full gap-3" onSubmit={handleSubmit}>
+                    <PrimaryInput name="title" placeholder="Nueva tarea" required />
 
-                            className="squircle flex flex-col gap-4 w-130 max-w-[90vw] p-10 rounded-3xl bg-surface-container-low"
-                        >
-
-                            <header className="flex flex-col">
-                                <div className="flex justify-between items-start">
-                                    <span className="text-3xl font-semibold tracking-tighter">Agregar tarea</span>
-                                    <TertiaryButton
-                                        Icon={<X strokeWidth={2.4} className="size-[1.1em]" />}
-                                        onClick={() => setShowModal(false)}
-                                        extraclass="text-ref-palette-neutral-50"
-                                    />
-                                </div>
-
-                                <span className="text-base font-light">Ingresa el nombre para tu nueva tarea</span>
-                            </header>
-
-                            <form className="flex flex-col w-full gap-3" onSubmit={handleSubmit}>
-                                <PrimaryInput name="title" label="Nombre de la tarea" placeholder="Nueva tarea" required />
-
-                                <PrimaryButton
-                                    text={isLoading ? "Guardando..." : "Crear tarea"}
-                                    extraclass="w-full"
-                                    type="submit"
-                                    disabled={isLoading}
-                                />
-                            </form>
-                        </motion.div>
-                    </motion.div>
-                )}
-            </AnimatePresence>
+                    <PrimaryButton
+                        text={isLoading ? "Guardando..." : "Crear tarea"}
+                        extraclass="w-full"
+                        type="submit"
+                        disabled={isLoading}
+                    />
+                </form>
+            </AnimatedModal>
         </>
     );
 }
